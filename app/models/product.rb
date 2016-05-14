696432d50e2c3dfd4d75ddf004d4d218daa85f0f
Daniel <truenorthtrainingcompany@gmail.com>
2016-05-13 21:37:44 -0400

    Product class for LCBO product Has methods for returning JSON data from LCBO api using HTTParty gem

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+class Product
+	include HTTParty
+	base_uri 'lcboapi.com'
+
+	def self.all
+		get('/products')['result']
+	end
+
+	def self.find(product_id)
+		get("/products/#{product_id}")['result']
+	end
+
+end
